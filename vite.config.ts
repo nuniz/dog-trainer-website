a68b85d5e6b42abacbc1f3452dc88ac3f27c9588
zorea <zoreasaf@gmail.com>
2025-06-21 13:32:07 +0300

    change base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/dog-trainer-website-main/",
+  base: "/dog-trainer-website/",
   server: {
     host: "::",
     port: 8080,
